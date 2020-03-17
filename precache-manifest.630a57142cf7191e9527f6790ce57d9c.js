6ac700661c1e741715457b3a80aa4b16effde02f
Onur Taşçı <onurtasci@Onur-MacBook-Pro.local>
2020-03-17 22:17:14 +0300

    Updates

---
+++ precache-manifest.630a57142cf7191e9527f6790ce57d9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3dbfa1549efbaff097d8391d31095d",
+    "url": "/turing/index.html"
+  },
+  {
+    "revision": "8c561bdd2f5f573b9724",
+    "url": "/turing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6ab6aa4ed4468cad3eb0",
+    "url": "/turing/static/js/2.b8d72a55.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/turing/static/js/2.b8d72a55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c561bdd2f5f573b9724",
+    "url": "/turing/static/js/main.ab2443ff.chunk.js"
+  },
+  {
+    "revision": "a973540f116c6b77c3d5",
+    "url": "/turing/static/js/runtime-main.bd593dfc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/turing/static/media/logo.5d5d9eef.svg"
+  }
+]);
